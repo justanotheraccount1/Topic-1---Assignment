d864c0d693ce0c15147197bfdd38a0473bb4ad8b
339729139 <339729139@2260I-L29-009>
2024-09-05 09:32:59 -0400

    Add project files.

---
+++ Topic 1 - Assignment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Topic_1___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sam Ahrens Sept/05/24
+            Console.WriteLine("  "
+        }
+    }
+}
