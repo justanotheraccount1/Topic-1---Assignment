8d012a43580731e44bd9bd750e4dc2e055d99d25
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-06 09:17:51 -0400

    Finished Project

--- Topic 1 - Assignment/Program.cs
+++ Topic 1 - Assignment/Program.cs
@@ -4,8 +4,34 @@
     {
         static void Main(string[] args)
         {
-            //Sam Ahrens Sept/05/24
-            Console.WriteLine("  "
+            //Sam Ahrens 09/05/24
+            //People drive cars to go places faster than if they walked
+            Console.WriteLine("Hello, my name is Sam.");
+            Console.WriteLine();
+            Console.WriteLine("Comments are used to comunicate the purpose of code to other programmers.");
+            Console.WriteLine();
+            Console.Write(" SSS  ");
+            Console.Write("  A   ");
+            Console.WriteLine("M   M");
+            Console.Write("S   S ");
+            Console.Write(" A A  ");
+            Console.WriteLine("MM MM");
+            Console.Write("S     ");
+            Console.Write("A   A ");
+            Console.WriteLine("MM MM");
+            Console.Write(" SSS  ");
+            Console.Write("AAAAA ");
+            Console.WriteLine("M M M");
+            Console.Write("    S ");
+            Console.Write("A   A ");
+            Console.WriteLine("M   M");
+            Console.Write("S   S ");
+            Console.Write("A   A ");
+            Console.WriteLine("M   M");
+            Console.Write(" SSS  ");
+            Console.Write("A   A ");
+            Console.WriteLine("M   M");
+            Console.ReadLine();
         }
     }
 }
